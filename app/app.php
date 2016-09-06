2d1f50318372f68070f06adc60c1562161b22dc8
April Peng and J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-06 09:39:19 -0700

    Add hello and goodbye to app file.

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+
+    $app = new Silex\Application();
+
+    $app->get("/hello", function() {
+        return "Hello friend!";
+    });
+
+    $app->get("/goodbye", function() {
+        return "Goodbye friend!";
+    });
+
+    return $app;
+?>
